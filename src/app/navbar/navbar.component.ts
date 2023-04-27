83a65ad2170b6dd9facf9d8aac79845726a89e49
arthur resende faria <arthurresendefaria@gmail.com>
2023-04-27 17:28:07 -0300

    Feature: Remove theme toggle

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,26 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-
-  lightMode = true
-
-  changeTheme() {
-    let current_mode = '';
-    let new_mode = '';
-
-    if (this.lightMode === true) {
-      current_mode = 'lightmode';
-      new_mode = 'darkmode';
-    } else {
-      current_mode = 'darkmode';
-      new_mode = 'lightmode';
-    }
-
-    let elements = document.querySelectorAll('.'+current_mode);
-    elements.forEach(element => {
-      element.setAttribute("class", new_mode)
-    });
-
-    this.lightMode = !this.lightMode;
-  }
 }
